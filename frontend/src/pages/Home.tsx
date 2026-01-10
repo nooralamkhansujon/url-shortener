ee569bbd3403cc4e7aee425715702ecf81cc2c5c
nooralamkhansujon <nooralamkhansujon@gmail.com>
2026-01-10 19:19:02 +0600

    feat: now if user visit url then redirect to login page if not authenticated else direct to dashboarda

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,22 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuthStore } from "../store/useAuthStore";
+
 export default function Home() {
+  const navigate = useNavigate();
+  const token = useAuthStore((state) => state.token);
+
+  useEffect(() => {
+    if (token) {
+      navigate("/dashboard", { replace: true });
+    } else {
+      navigate("/login", { replace: true });
+    }
+  }, [token, navigate]);
+
   return (
     <div className="flex h-screen items-center justify-center">
-      <h1 className="text-3xl font-bold">React Frontend Ready 🚀</h1>
+      <h1 className="text-3xl font-bold">Redirecting...</h1>
     </div>
   );
 }
